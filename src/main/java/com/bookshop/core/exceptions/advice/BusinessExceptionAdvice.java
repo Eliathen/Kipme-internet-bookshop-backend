04dac0935f37dc149a97f97c2aa86876a05ada82
Marek Szymanski <szymanski_m@o2.pl>
2023-11-28 14:59:16 +0100

    Added exception handler for MethodArgumentNotValidException to handle exceptions from controllers' requests

--- src/main/java/com/bookshop/core/exceptions/advice/BusinessExceptionAdvice.java
+++ src/main/java/com/bookshop/core/exceptions/advice/BusinessExceptionAdvice.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -50,5 +52,22 @@ public class BusinessExceptionAdvice {
         logger.error(message);
         ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), Collections.singletonList(message));
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorInfo> argumentsNotValid(MethodArgumentNotValidException exception) {
+        List<String> errorMessage = getErrorMessagesFromException(exception);
+        for (String message : errorMessage) {
+            logger.error(message);
+        }
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), errorMessage);
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+
+    private List<String> getErrorMessagesFromException(MethodArgumentNotValidException exception) {
+        return exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
     }
 }
