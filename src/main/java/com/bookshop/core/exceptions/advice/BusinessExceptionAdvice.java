01bdcbffc3a2bb6eef3a2cba91ad3cc8ef13a72d
Marek Szymanski <szymanski_m@o2.pl>
2023-11-28 14:57:35 +0100

    Changed message type in ErrorInfo to List<String>, added logging to existing handlers

--- src/main/java/com/bookshop/core/exceptions/advice/BusinessExceptionAdvice.java
+++ src/main/java/com/bookshop/core/exceptions/advice/BusinessExceptionAdvice.java
@@ -5,37 +5,50 @@ import com.bookshop.core.exceptions.base.ResourceAlreadyExists;
 import com.bookshop.core.exceptions.base.ResourceNotFoundException;
 import com.bookshop.features.user.exception.InvalidEmailAddress;
 import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
 @ControllerAdvice
 public class BusinessExceptionAdvice {
 
+    private final Logger logger = LoggerFactory.getLogger(BusinessExceptionAdvice.class);
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorInfo> notFound(ResourceNotFoundException exception) {
-        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), exception.getMessage());
+        String message = exception.getMessage();
+        logger.error(message);
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), Collections.singletonList(message));
         return new ResponseEntity<>(errorInfo, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceAlreadyExists.class)
     public ResponseEntity<ErrorInfo> alreadyExists(ResourceAlreadyExists exception) {
-        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), exception.getMessage());
+        String message = exception.getMessage();
+        logger.error(message);
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), Collections.singletonList(message));
         return new ResponseEntity<>(errorInfo, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorInfo> constraintViolation(ConstraintViolationException exception) {
-        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), exception.getMessage());
+        String message = exception.getMessage();
+        logger.error(message);
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), Collections.singletonList(message));
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidEmailAddress.class)
     public ResponseEntity<ErrorInfo> invalidEmailAddress(InvalidEmailAddress exception) {
-        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), exception.getMessage());
+        String message = exception.getMessage();
+        logger.error(message);
+        ErrorInfo errorInfo = new ErrorInfo(LocalDateTime.now(), Collections.singletonList(message));
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 }
