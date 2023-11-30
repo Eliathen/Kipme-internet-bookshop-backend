daff227c7eb7c9986046acc4c0f7dba2b61c477c
Marek Szymanski <szymanski_m@o2.pl>
2023-11-30 13:27:06 +0100

    Added unit tests for PublisherMapper

---
+++ src/test/java/com/bookshop/features/book/mapper/PublisherMapperTest.java
@@ -0,0 +1,38 @@
+package com.bookshop.features.book.mapper;
+
+import com.bookshop.features.book.api.request.SavePublisherRequest;
+import com.bookshop.features.book.data.entity.PublisherEntity;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PublisherMapperTest {
+
+    @Test
+    void shouldMapPublisherEntityToPublisherResponseCorrect() {
+        var entity = PublisherEntity.builder()
+                .id(1)
+                .publisherName("Publisher 1")
+                .publisherCity("City 1")
+                .publisherBooks(Collections.emptySet())
+                .build();
+
+        var response = PublisherMapper.mapToPublisherResponse(entity);
+
+        assertThat(response.getId()).isEqualTo(entity.getId());
+        assertThat(response.getPublisherName()).isEqualTo(entity.getPublisherName());
+        assertThat(response.getPublisherCity()).isEqualTo(entity.getPublisherCity());
+    }
+
+    @Test
+    void shouldMapSavePublisherRequestToPublisherEntityCorrect() {
+        var request = new SavePublisherRequest("Publisher 1", "City 1");
+
+        var entity = PublisherMapper.mapToPublisher(request);
+
+        assertThat(entity.getPublisherName()).isEqualTo(request.getPublisherName());
+        assertThat(entity.getPublisherCity()).isEqualTo(request.getPublisherCity());
+    }
+}
